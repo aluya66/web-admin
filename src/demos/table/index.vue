59acbd811487aee8f7410d1764695353a5fa9324
zegang <redx123@126.com>
2019-10-16 19:20:47 +0800

    解决冲突引起文件丢失问题

--- src/demos/table/index.vue
+++ src/demos/table/index.vue
@@ -1,151 +1,279 @@
 <template>
-	<c-table
-		selection
-		:table-header="headerList"
-		:table-list="tableData"
-		:table-inner-btns="tableInnerBtns"
-		:current-page="curPage"
-		@change-page="changePage"
-		@choose-item="chooseItem"
-		@inner-handle="innerHandle"
-	>
-		<template v-slot:header>
-			<el-form :inline="true" :model="formInline" class="search">
-				<el-form-item label="审批人">
-					<el-input v-model="formInline.user" placeholder="审批人" />
-				</el-form-item>
-				<el-form-item label="活动区域">
-					<el-select v-model="formInline.region" placeholder="活动区域">
-						<el-option label="区域一" value="shanghai" />
-						<el-option label="区域二" value="beijing" />
-					</el-select>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" :loading="searchLoading" @click="onSearch">
-						查询
-					</el-button>
-				</el-form-item>
-			</el-form>
-
-			<el-button-group>
-				<el-button size="small" type="primary" icon="el-icon-plus">
-					新增
-				</el-button>
-				<el-button size="small" type="primary" icon="el-icon-delete">
-					删除
-				</el-button>
-				<el-button size="small" type="primary" icon="el-icon-search">
-					搜索
-				</el-button>
-			</el-button-group>
-		</template>
-	</c-table>
+  <c-view>
+    <template v-slot:header>
+      <div class="title">{{ $route.meta.name || $t(`route.${$route.meta.title}`) }}</div>
+    </template>
+    <div class="main__box">
+      <c-table
+        selection
+        hasBorder
+        :fixed-height="450"
+        :size="size"
+        :loading="isLoading"
+        :table-header="tableHeader"
+        :table-list="tableList"
+        :page-info="pageInfo"
+        :table-inner-btns="tableInnerBtns"
+        @change-pagination="changePagination"
+      >
+        <template v-slot:header>
+          <el-form :inline="true" :model="searchObj" label-width="100px" class="search-form">
+            <el-form-item label="商品名称">
+              <el-input
+                v-model="searchObj.goodsName"
+                class="search-item"
+                :size="size"
+                placeholder="请输入商品名称"
+                clearable
+              />
+            </el-form-item>
+            <el-form-item label="商品编码">
+              <el-input
+                v-model="searchObj.goodsBn"
+                class="search-item"
+                :size="size"
+                placeholder="请输入商品编码"
+                clearable
+              />
+            </el-form-item>
+            <el-form-item label="商品类目">
+              <el-input
+                v-model="searchObj.categoryCode"
+                class="search-item"
+                :size="size"
+                placeholder="请输入商品类目"
+                clearable
+              />
+            </el-form-item>
+            <!-- <el-form-item label="经营类型">
+              <el-input
+                v-model="searchObj.businessValue"
+                class="search-item"
+                :size="size"
+                placeholder="请输入操作人"
+                clearable
+              />
+            </el-form-item>-->
+            <el-form-item label="品牌">
+              <el-input
+                v-model="searchObj.brandName"
+                class="search-item"
+                :size="size"
+                placeholder="请输入品牌"
+                clearable
+              />
+            </el-form-item>
+            <el-form-item label="上下架">
+              <el-select
+                v-model="searchObj.marketable"
+                :size="size"
+                class="search-item"
+                placeholder="请选择"
+                clearable
+              >
+                <el-option
+                  v-for="item in marketableSelect"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="操作时间">
+              <el-date-picker
+                :size="size"
+                v-model="searchObj.dataTime"
+                type="daterange"
+                :picker-options="pickerOptions"
+                range-separator="至"
+                start-placeholder="开始时间"
+                end-placeholder="结束时间"
+                :default-time="['00:00:00', '23:59:59']"
+              >align="right"></el-date-picker>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                type="primary"
+                class="search-btn"
+                :size="size"
+                icon="el-icon-search"
+                @click="searchSubmit"
+              >查询</el-button>
+            </el-form-item>
+          </el-form>
+        </template>
+      </c-table>
+    </div>
+  </c-view>
 </template>
 
 <script>
-import CTable from 'components/table'
+import mixinTable from 'mixins/table'
+import utils from 'utils'
 
 export default {
-  name: 'List',
-
-  components: {
-    CTable
-  },
-  data () {
+  mixins: [mixinTable],
+  data(vm) {
     return {
-      formInline: {
-        user: '',
-        region: ''
+      searchObj: {
+        // businessValue: '',
+        categoryCode: '',
+        goodsBn: '',
+        goodsName: '',
+        marketable: '',
+        brandName: '',
+        dataTime: ''
       },
-      searchLoading: true,
-      tableInnerBtns: [
-        {
-          name: '查看',
-          handle: 'view'
-        },
-        {
-          name: '编辑',
-          handle: 'edit'
-        },
-        {
-          name: '删除',
-          handle: 'detele'
+      marketableSelect: [{
+        value: '1',
+        label: '上架'
+      },
+      {
+        value: '2',
+        label: '下架'
+      }],
+      pickerOptions: utils.pickerOptions,
+      tableInnerBtns: [{
+        width: 100,
+        name: '编辑',
+        icon: 'el-icon-edit',
+        handle(row) {
+          vm.routerLink(`/goods/detail/${row.id}`)
         }
-      ],
-      headerList: [
+      }],
+      tableHeader: [
         {
-          label: '日期',
-          prop: 'date'
+          label: '商品ID',
+          prop: 'id',
+          width: 120,
+          fixed: true
         },
         {
-          label: '姓名',
-          prop: 'name'
+          label: '商品名称',
+          prop: 'goodsName',
+          width: 200
         },
         {
-          label: '地址',
-          prop: 'address',
-          inline: true
+          label: '商品图片',
+          prop: 'coverImg',
+          width: 100,
+          isImage: true
+        },
+        {
+          label: '商品编码',
+          prop: 'goodsBn'
+        },
+        {
+          label: '商品类目',
+          prop: 'categoryName'
+        },
+        {
+          label: '品牌',
+          prop: 'brandName'
+        },
+        {
+          label: '库存',
+          prop: 'retailPrice',
+          width: 100
+        },
+        {
+          label: '成本价',
+          prop: 'marketable',
+          width: 100
+        },
+        {
+          label: '市场价',
+          prop: 'memberPrice',
+          width: 100
+        },
+        {
+          label: '销售价',
+          prop: 'mktPrice',
+          width: 100
+        },
+        {
+          label: '上架状态',
+          prop: 'marketable',
+          width: 100,
+          formatter(row) {
+            return row.marketable ? vm.marketableSelect[row.marketable - 1].label : ''
+          }
+        },
+        {
+          label: '上货人',
+          prop: 'tagPrice',
+          width: 100
+        },
+        {
+          label: '创建时间',
+          prop: 'created',
+          width: 100
+        },
+        {
+          label: '更新时间',
+          prop: 'updated',
+          width: 100
         }
-      ],
-      tableData: [
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-08',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-06',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-07',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }
-      ],
-      curPage: 1
+      ]
     }
   },
-
+  created() {
+    this.fetchData()
+  },
   methods: {
-    changePage (page) {
-      console.log(page)
+    /**
+     * 获取表格数据
+    */
+    fetchData() {
+      const { dataTime, ...other } = this.searchObj
+      const { totalNum, ...page } = this.pageInfo
+      const searchDate = this.getSearchDate(dataTime)
+      this.isLoading = true
+      this.$api.goods.getList(
+        {
+          ...searchDate,
+          ...other,
+          ...page
+        }
+      ).then(res => {
+        this.isLoading = false
+        if (res.totalCount) {
+          const { data, totalCount } = res
+          this.pageInfo.totalNum = totalCount
+          this.tableList = data
+        } else {
+          this.tableList = res
+        }
+      })
     },
-    chooseItem (val) {
-      console.log('val', val)
-    },
-    onSearch () {
-      console.log('submit!')
-    },
-    innerHandle (type, item) {
-      console.log(type, item)
+    /**
+     * 删除表格单条数据
+     *
+     * @param {*} curPromise
+     * @param {string} [msgTip='删除成功']
+     */
+    deleteData(param, msgTip = '删除成功') {
+      this.$api.basic.deleteBrand(param).then(() => {
+        this.$msgTip(msgTip)
+        if (this.tableList.length === 1) {
+          const { pageNum } = this.pageInfo
+          this.pageInfo.pageNum = pageNum > 1 ? pageNum - 1 : 1
+        }
+        this.fetchData()
+      })
     }
   }
 }
 </script>
+
 <style lang="less" scoped>
-.table-button {
-	margin: 20px 0;
+.main__box {
+  .search-form {
+    margin-bottom: 10px;
+    width: 100%;
+    .search-item {
+      width: 250px;
+    }
+  }
 }
 </style>
---
+++ src/views/layout/components/sidebar/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<div :class="{ 'has-logo': showLogo }">
+		<logo v-if="showLogo" :collapse="isCollapse" />
+		<el-scrollbar wrap-class="scrollbar-wrapper">
+      <!-- background-color="#304156"
+				text-color="#bfcbd9"
+				active-text-color="#409EFF" -->
+			<el-menu
+				:default-active="$route.path"
+				:collapse="isCollapse"
+				:collapse-transition="false"
+				mode="vertical"
+			>
+				<sidebar-item
+					v-for="route in permission_routes"
+					:key="route.path"
+					:item="route"
+					:base-path="route.path"
+				/>
+			</el-menu>
+		</el-scrollbar>
+	</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Logo from './Logo'
+import SidebarItem from './SidebarItem'
+// import variables from "styles/variables.less";
+
+export default {
+  components: { SidebarItem, Logo },
+  computed: {
+    ...mapGetters(['permission_routes', 'sidebar']),
+    showLogo () {
+      return this.$store.state.settings.sidebarLogo
+    },
+    // variables() {
+    //   return variables;
+    // },
+    isCollapse () {
+      return !this.sidebar.opened
+    }
+  }
+}
+</script>
