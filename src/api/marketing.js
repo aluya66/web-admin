8a85c5f1458ee19eb2c8fecd227a9dd8f4b5548e
zegang <redx123@126.com>
2019-10-25 18:53:24 +0800

    添加营销中心上下文

--- src/api/marketing.js
+++ src/api/marketing.js
@@ -1,6 +1,6 @@
 import Http from '../utils/request'
 
-const context = process.env.VUE_APP_CONSOLE_COUPON
+const context = process.env.VUE_APP_CONSOLE_MARKETING
 
 // 店铺列表
 export const getCoupon = params =>
