4c4f3702be2633a93cd1e8fb7e04c260ebf726a8
chenjiapeng <398569293@qq.com>
2019-12-09 11:33:15 +0800

    活动折扣

---
+++ src/views/marketing/discount/detail/base.vue
@@ -0,0 +1,319 @@
+<template>
+  <c-card :name="title" class="form-card" id="form-base">
+    <el-form-item label="活动名称:" prop="goodsName">
+      <el-input
+        disabled
+        class="select-item"
+        v-model.trim="formModel.goodsName"
+        :size="size"
+        placeholder="请输入活动名称"
+        clearable
+      />
+    </el-form-item>
+    <el-form-item label="活动类型:" prop="businessValue">
+      <el-select
+        v-if="!isView"
+        class="select-item"
+        :disabled="isDisabled"
+        v-model="formModel.goodsBusinessId"
+        placeholder="请选择经营类型"
+      >
+        <el-option
+          v-for="item in businessArr"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="使用渠道:" prop="businessValue">
+      <el-select
+        v-if="!isView"
+        class="select-item"
+        :disabled="isDisabled"
+        v-model="formModel.goodsBusinessId"
+        placeholder="请选择经营类型"
+      >
+        <el-option
+          v-for="item in businessArr"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="活动时间类型:" prop="businessValue">
+      <el-select
+        v-if="!isView"
+        class="select-item"
+        :disabled="isDisabled"
+        v-model="formModel.goodsBusinessId"
+        placeholder="请选择经营类型"
+      >
+        <el-option
+          v-for="item in businessArr"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="设置固定周期:" prop="created">
+      <el-date-picker
+        disabled
+        v-model="formModel.created"
+        value-format="yyyy-MM-dd HH:mm:ss"
+        type="dateTime"
+        class="select-item"
+        placeholder="选择日期"
+      ></el-date-picker>
+    </el-form-item>
+    <el-form-item label="设置固定月份:" prop="businessValue">
+      <el-select
+        v-if="!isView"
+        class="select-item"
+        :disabled="isDisabled"
+        v-model="formModel.goodsBusinessId"
+        placeholder="请选择经营类型"
+      >
+        <el-option
+          v-for="item in businessArr"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="设置固定日期:" prop="businessValue">
+      <el-select
+        v-if="!isView"
+        class="select-item"
+        :disabled="isDisabled"
+        v-model="formModel.goodsBusinessId"
+        placeholder="请选择经营类型"
+      >
+        <el-option
+          v-for="item in businessArr"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+    </el-form-item>
+  </c-card>
+</template>
+<script>
+import CCard from 'components/card'
+import utils from 'utils'
+
+export default {
+  data() {
+    return {
+      businessArr: [{
+        label: '自营',
+        value: 1
+      }],
+      goodsTypeArr: [{
+        label: '服装',
+        value: 1
+      }],
+      originList: [{
+        label: '门店挂板',
+        value: 1
+      }, {
+        label: '代销商品',
+        value: 2
+      }, {
+        label: '自营商品',
+        value: 3
+      }],
+      categoryList: [],
+      brandList: [],
+      formModel: {}
+    }
+  },
+  props: {
+    title: String,
+    isTag: {
+      type: Boolean,
+      default: false
+    },
+    dataObj: {
+      type: Object,
+      required: true
+    },
+    isView: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: String,
+      default: 'medium'
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  created() {
+    this.getCategoryList()
+    this.initData()
+    if (!this.isTag) {
+      this.getbrandList()
+    }
+  },
+  methods: {
+    initData() {
+      const {
+        goodsName, // 商品图片
+        operationName, // 运营名称
+        categoryCode, // 分类code
+        categoryName, // 分类名称
+        goodsBn, // 商品款号
+        supplierCode, // 供应商款号
+        supplierName, // 供应商
+        brandName, // 商品品牌
+        goodsStyleName, // 款式来源
+        saleYear, // 年份
+        season, // 季节
+        sourceSupplierName, // 货源商
+        coverImg, // 识别图/封面图
+        marketable, // 是否可售
+        updatebyName, // 上货人
+        skus,
+        updated,
+        created
+        // categoryCode,
+        // businessValue,
+        // goodsTypeId,
+        // brandName,
+        // brandId,
+        // goodsName,
+        // goodsShortName,
+        // goodsStaticFiles,
+        // goodsBrief,
+        // goodsBn,
+        // origin,
+        // categoryName,
+        // skus,
+        // updated,
+        // created
+      } = this.dataObj
+      let seasonCN = ''
+      switch (season) {
+        case 1:
+          seasonCN = '春'
+          break
+        case 2:
+          seasonCN = '夏'
+          break
+        case 3:
+          seasonCN = '秋'
+          break
+        case 4:
+          seasonCN = '冬'
+          break
+      }
+      let skuList = [] // 商品打标签下 sku 图片列表
+      if (this.isTag) {
+        skus && skus.forEach(({ imageUrl }) => {
+          skuList.push(imageUrl)
+        })
+        skuList = utils.uniqueArr(skuList) // 去重sku图片
+      }
+
+      const curCategoryCode = []
+      if (categoryCode) {
+        const codeLen = categoryCode.split('')
+        codeLen.forEach((res, index) => {
+          if (index % 2 === 0 && index + 2 <= codeLen.length) {
+            curCategoryCode.push(categoryCode.substr(0, index + 2))
+          }
+        })
+      }
+      this.formModel = {
+        goodsName, // 商品图片
+        operationName, // 运营名称
+        categoryCode, // 分类code
+        categoryName, // 分类名称
+        goodsBn, // 商品款号
+        supplierCode, // 供应商款号
+        supplierName, // 供应商
+        brandName, // 商品品牌
+        goodsStyleName, // 款式来源
+        saleYear, // 年份
+        season: seasonCN, // 季节
+        sourceSupplierName, // 货源商
+        coverImg, // 识别图/封面图
+        marketable, // 是否可售
+        updatebyName,
+        skus,
+        updated,
+        created
+      }
+    },
+    getCategoryList() {
+      this.$api.basic.queryCategoryAll().then(res => {
+        this.categoryList = utils.formartLevelData(res)
+      })
+    },
+    getbrandList() {
+      this.$api.basic.brandList({
+        pageNo: 1,
+        pageSize: 100,
+        status: 1
+      }).then(res => {
+        const { data, totalCount } = res
+        if (totalCount) {
+          this.brandList = data.map(val => ({
+            label: val.name,
+            value: val.id
+          }))
+        }
+      })
+    },
+    uploadSuccess(response, file, fileList) {
+      this.fileList = fileList
+    },
+    uploadRemove(file, fileList) {
+      this.fileList = fileList
+    },
+    uploadReview(file) {
+      this.$emit('show-image', file)
+    }
+  },
+  components: {
+    CCard
+  }
+}
+</script>
+<style lang="less" scoped>
+.form-card {
+  .el-form-item {
+    width: 98%;
+    margin-bottom: 15px;
+  }
+  .select-item {
+    width: 30%;
+  }
+  .pic {
+    padding-bottom: 25px;
+    span {
+      display: inline-block;
+      margin-bottom: 10px;
+    }
+    .info {
+      line-height: 20px;
+      height: 20px;
+      margin-top: 5px;
+      font-size: @f12;
+    }
+  }
+  .coverImg {
+    width: 200px;
+    height: 200px;
+    display: inline-block;
+    margin-right: 5px;
+  }
+}
+</style>
---
+++ src/views/marketing/discount/detail/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <c-view>
+    <template v-slot:header>
+      <el-page-header
+        @back="goBack"
+        :content="$route.meta.name || $t(`route.${$route.meta.title}`)"
+      ></el-page-header>
+    </template>
+    <div class="point-wrapper">
+      <div
+        class="point-item"
+        v-for="(item, index) in stepList"
+        :key="index"
+        v-point="item.id"
+        :class="{'current-point': currentPoint === index}"
+        @click="selectPoint(index)"
+      >
+        <div class="point-box">
+          <div class="circle">{{index + 1}}</div>
+          <div class="line" v-show="index !== stepList.length-1"></div>
+        </div>
+        <div class="point-name">{{item.name}}</div>
+      </div>
+    </div>
+    <el-form
+      ref="formRef"
+      :model="formModel"
+      label-width="120px"
+      class="form"
+      label-position="right"
+    >
+      <g-basic
+        :is-view="isView"
+        :is-disabled="isDisabled"
+        :data-obj="formModel"
+        v-if="formModel.id"
+        ref="basicRef"
+        title="基础信息"
+        @show-image="reviewImage"
+      ></g-basic>
+      <el-form-item class="form-btn">
+        <el-button :loading="btnLoading" type="primary" @click.native.prevent="submitHandle">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </c-view>
+</template>
+
+<script>
+import MixinForm from 'mixins/form'
+import GBasic from './basic'
+
+export default {
+  name: 'discountDetail',
+  mixins: [MixinForm],
+  data() {
+    return {}
+  },
+
+  created() {
+
+  },
+
+  methods: {
+  },
+
+  components: {
+    GBasic
+  }
+}
+</script>
+<style lang='less' scoped>
+.form {
+  background-color: @white;
+  padding: 15px 15px;
+  .form-btn {
+    margin-left: 20px;
+    margin-top: 20px;
+  }
+}
+</style>
