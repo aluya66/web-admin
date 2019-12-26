c5f2096f85d504cbb04fed8b916fcdab48d9017b
chenjiapeng <398569293@qq.com>
2019-12-26 23:06:11 +0800

    商品选择组件调整

--- src/views/common/goodsSelect.vue
+++ src/views/common/goodsSelect.vue
@@ -2,7 +2,10 @@
   <div class="goods-select">
     <div class="header">
       <div class="title title-left">商品列表</div>
-      <div class="title title-right">已选商品:【 {{checkedAttr.length}} 】<el-button @click="deleteAll">删除所有</el-button></div>
+      <div class="title title-right">
+        已选商品:【 {{checkedAttr.length}} 】
+        <el-button @click="deleteAll">删除所有</el-button>
+      </div>
     </div>
     <div class="content">
       <div class="source">
@@ -312,12 +315,25 @@ export default {
     handleSkuList(rows) {
       if (!rows.length) return
       // 选中的sku是否属于已选择的商品, 选择的sku属于同一商品所以只需要拿数组【0】去对比
-      const idx = this.checkedAttr.findIndex((selectedGood) => selectedGood.goodsBn === rows[0].goodsBn)
+      const idx = this.checkedAttr.findIndex((selectedGood) => {
+        if (selectedGood.goodsBn === rows[0].goodsBn) {
+          if (selectedGood.shopId === rows[0].shopId) {
+            return true
+          }
+        }
+      })
+      console.log(idx)
       if (idx !== -1) { // 商品已选择， 在已选中商品列表中添加sku集合
         this.checkedAttr[idx].skuList = rows // 设置已选中的sku集合
         this.checkedAttr = JSON.parse(JSON.stringify(this.checkedAttr))
       } else { // 商品未被选择， 添加标识 商品未被选中，只选择sku isSelected:false
-        const goodIndex = this.tableList.findIndex((good) => good.goodsBn === rows[0].goodsBn)
+        const goodIndex = this.tableList.findIndex((good) => {
+          if (good.goodsBn === rows[0].goodsBn) {
+            if (good.shopId === rows[0].shopId) {
+              return true
+            }
+          }
+        })
         if (goodIndex !== -1) {
           // this.tableList[goodIndex] = this.tableList[goodIndex].map((item) => ({ ...item, isSelected: false }))
           Object.assign(this.tableList[goodIndex], { skuList: rows, isSelected: false })
