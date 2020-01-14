751dd25bb0ced3e7efda015cf28256cae117c5d7
chenjiapeng <398569293@qq.com>
2020-01-14 20:25:09 +0800

    商品导出功能开发

--- src/views/goods/manage/list/index.vue
+++ src/views/goods/manage/list/index.vue
@@ -13,9 +13,8 @@
         v-show="activeName === 'perfect'"
         :size="size"
         type="primary"
-        :loading="exportLoading"
         icon="el-icon-download"
-        @click="exportFile"
+        @click="handleGoods"
       >导出</el-button>
       <!-- <div class="header-btn">
         <el-button
@@ -31,6 +30,17 @@
         <component ref="childList" v-bind:is="activeName" @update-count="updateCount"></component>
       </keep-alive>
     </div>
+    <div v-if="dialogShow">
+      <c-dialog
+        :is-show="dialogShow"
+        title="提示"
+        close-btn
+        noBtn
+        @before-close="dialogShow = false"
+      >
+        <p>您导出的商品数据量较大，请等待5分钟后，在 <em class="router-link" @click="toExportList">基础服务-导出列表</em> 查看下载！</p>
+      </c-dialog>
+    </div>
   </c-view>
 </template>
 <script>
@@ -38,17 +48,18 @@ import mixinTable from 'mixins/table'
 import noperfect from './noperfect'
 import perfect from './perfect'
 import utils from 'utils'
-
+import CDialog from 'components/dialog'
 export default {
   name: 'goodsManage',
   components: {
     noperfect,
-    perfect
+    perfect,
+    CDialog
   },
   mixins: [mixinTable],
   data(vm) {
     return {
-      exportLoading: false,
+      dialogShow: false,
       noperfectCount: 0, // 未完善商品数量
       activeName: 'perfect',
       tagTypeList: [], // 标签类型数据
@@ -65,42 +76,13 @@ export default {
     }
   },
   methods: {
-    exportFile() {
-      const { createDateTime, updateDateTime, categoryCode, stock, ...other } = this.$refs.childList.searchObj
-      const createDate = this.getSearchDate(createDateTime, '', 'beginDate', 'endDate')
-      const updateDate = this.getSearchDate(updateDateTime, '', 'updateBeginDate', 'updateEndDate')
-      const categoryVal = {
-        cateCodeFirst: categoryCode[0] || '',
-        cateCodeSecond: categoryCode[1] || '',
-        cateCodeThird: categoryCode[2] || ''
-      }
-      if (utils.isInteger(stock[0]) || utils.isInteger(stock[1])) {
-        return this.$msgTip('商品库存请输入正整数', 'warning')
-      }
-      if (stock[0] > stock[1]) {
-        return this.$msgTip('库存最小值不能大于最大值', 'warning')
-      }
-
-      this.exportLoading = true
-      this.$api.goods.exportGoods({
-        ...createDate,
-        ...updateDate,
-        ...categoryVal,
-        ...other,
-        minStock: stock[0] || '',
-        maxStock: stock[1] || ''
-      }).then(res => {
-        this.exportLoading = false
-        if (res) {
-          const { data, filename } = res
-          if (data && filename) {
-            utils.createBlobFile(data, filename)
-          } else {
-            this.$msgTip('导出数据异常', 'warning')
-          }
-        } else {
-          this.$msgTip('导出数据失败', 'warning')
-        }
+    toExportList() {
+      this.dialogShow = false
+      this.routerLink('/basic/exportList')
+    },
+    handleGoods() {
+      this.$api.goods.exportGoods().then(res => {
+        if (res) this.dialogShow = true
       })
     },
     updateCount(val) {
@@ -109,3 +91,10 @@ export default {
   }
 }
 </script>
+
+<style lang="less">
+  .router-link {
+    color: @menuActive;
+    cursor: pointer;
+  }
+</style>
