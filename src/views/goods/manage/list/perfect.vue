f337fca16b6a05de35d2a63dd9040d71e60da7de
lvxueying <791009808@qqq.com>
2020-02-28 14:59:53 +0800

    [feat]():文案

--- src/views/goods/manage/list/perfect.vue
+++ src/views/goods/manage/list/perfect.vue
@@ -130,7 +130,7 @@ export default {
           prop: 'originName'
         },
         {
-          label: '平台净采购价',
+          label: '平台成本价',
           prop: 'sampleCostprice',
           search: {
             label: '更新时间',
